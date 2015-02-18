2ceed5f6b22a28458f45f54f8915ad602ba04548
Diego F. Reyes <diferrev@gmail.com>
2015-02-18 17:54:23 -0500

    Se inicia la maquetacion HTML

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="container">
+	<span>version</span>
+</div>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Recaudo de Efectivo de Giros</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<header class="header">
+	<div class="container">
+		<h1 class="header__title">Recaudo de Efectivo de Giros</h1>
+		<span class="header__subtitle">Apuestas Azar S.A.</span>
+	</div>
+</header>
